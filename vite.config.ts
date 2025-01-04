22d063e8c78a245856ad641f486f5abda8b0ee3f
Josh <hello@mrjoshball.com>
2025-01-04 10:43:39 -0500

    initial push

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    port: 5174,
+  },
+  base: process.env.NODE_ENV === 'production' ? '/email-template-generator/' : '/',
+});
